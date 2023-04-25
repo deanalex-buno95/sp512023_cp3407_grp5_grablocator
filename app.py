96fdaa7a32d6a3afaa7df41ab9ae40f7fcd06649
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-04-26 00:24:32 +0800

    Modify `.idea`

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def login_or_register():
+    return render_template("index.html")
 
 
-app.run()
+app.run(debug=True)
