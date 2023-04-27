d826d13cc4f4f6cfee5397ae4eaf02cf6bdc464a
davinnataz <davinnatanael@my.jcu.edu.au>
2023-04-27 23:43:15 +0800

    fix login to add some variable

--- login.py
+++ login.py
@@ -3,7 +3,10 @@ class Login:
     """
     create login database
     """
-    def __init__(self, username, password):
+    def __init__(self, first_name, last_name, ages, username, password):
+        self.firstname = first_name
+        self.lastname = last_name
+        self.ages = ages
         self.username = username
         self.password = password
 
