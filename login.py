9cc40633284f9dc6c89aaf4c1ea86c0991352f36
davinnataz <davinnatanael@my.jcu.edu.au>
2023-04-28 00:07:09 +0800

    change variable name

--- login.py
+++ login.py
@@ -3,10 +3,10 @@ class Login:
     """
     create login database
     """
-    def __init__(self, first_name, last_name, ages, username, password):
+    def __init__(self, first_name, last_name, ages, id, password):
         self.firstname = first_name
         self.lastname = last_name
         self.ages = ages
-        self.username = username
+        self.id = id
         self.password = password
 
--- login_db.py
+++ login_db.py
@@ -10,7 +10,7 @@ c.execute(''' CREATE TABLE login  (
             first_name text,
             last_name text,
             work_experience integer,
-            id integer, 
+            id integer,
             password text
 )''')
 
@@ -20,6 +20,8 @@ c.execute("INSERT INTO login ('Ja', 'Doe', 8, 98703012001, 'Testing3')")
 c.execute("INSERT INTO login ('James', 'Doe', 11, 98704012001, 'Testing4')")
 
 con.commit()
+
+
 
 con.close()
 
