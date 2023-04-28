37ed47420a59c4b62f96109626bc6bf57d28056f
davinnataz <davinnatanael@my.jcu.edu.au>
2023-04-28 12:57:10 +0800

    add address class

---
+++ address.py
@@ -0,0 +1,7 @@
+class Address:
+
+    def __init__(self, blk, street_num, unit_num, postal_code):
+        self.block = blk
+        self.streetnumber = street_num
+        self.unitnumber = unit_num
+        self.postal = postal_code
