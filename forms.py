d5079f9a8f5b1ee4c9ef91db60b19ef4ea46c9ef
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-04-29 02:19:53 +0800

    Update `register.html` to add the address fields.

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ forms.py
 
 from datetime import date, timedelta
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, EmailField, PasswordField, SubmitField, validators
+from wtforms import StringField, DateField, IntegerField, EmailField, PasswordField, SubmitField, validators
 
 
 class RegisterForm(FlaskForm):
@@ -15,26 +15,49 @@ class RegisterForm(FlaskForm):
     Flask Form to register a new driver into the database.
     """
     # All inputs in a register form.
+
+    # # ID
     driver_id = StringField('ID',
                             validators=[validators.InputRequired(),
                                         validators.Length(min=9, max=9)])
+
+    # # Name
     driver_name = StringField('Name',
                               validators=[validators.InputRequired()])
+
+    # # Date Of Birth
     driver_dob = DateField('Date Of Birth',
                            format='%Y-%m-%d',
                            validators=[validators.InputRequired()])
+
+    # # Hire Date
     driver_hire_date = DateField('Hire Date',
                                  format='%Y-%m-%d',
                                  validators=[validators.InputRequired()])
+
+    # # Address
+    driver_address_block_number = IntegerField('Block/House Number', validators=[validators.InputRequired()])
+    driver_address_unit_floor_number = IntegerField('Unit Floor', validators=[validators.Optional(), validators.NumberRange(min=1, max=50)])
+    driver_address_unit_apartment_number = IntegerField('Apartment Number', validators=[validators.Optional()])
+    driver_address_street = StringField('Street', validators=[validators.InputRequired()])
+    driver_address_postal_code = IntegerField('Postal Code', validators=[validators.InputRequired(), validators.NumberRange(min=100000, max=999999)])
+
+    # # Plate Number
     driver_plate_number = StringField('Plate Number',
                                       validators=[validators.InputRequired()])
+
+    # # Email
     driver_email = EmailField('Email',
                               validators=[validators.InputRequired()])
+
+    # # Password
     driver_new_password = PasswordField('New Password',
                                         validators=[validators.InputRequired(),
                                                     validators.Length(min=8)])
     driver_confirm_password = PasswordField('Confirm Password',
                                             validators=[validators.EqualTo('driver_new_password')])
+
+    # # Submit
     form_submit = SubmitField('Register')
 
     # Validators
