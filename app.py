7737e7c9297de3c510e5557dacb397d25f49e9d5
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-04-28 23:45:55 +0800

    Update `app.py`.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session
 
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "c2e3QoE'Ug)C--xJ(SHu?+R+9bd^ap"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
 
 
 @app.route('/')
-def login_or_register():
+def index():
     return render_template("index.html")
-
 
 app.run(debug=True)
