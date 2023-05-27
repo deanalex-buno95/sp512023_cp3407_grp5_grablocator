f0708fd940366945da38f85341cd337b8a3cb4db
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-05-28 02:02:25 +0800

    Add a function in `script.js` to get the current year for the display of copyright before displaying on `layout.html` and other HTML files.

---
+++ static/script.js
@@ -0,0 +1,15 @@
+/**
+ * JavaScript functions.
+ *
+ * script.js
+ */
+
+
+/* Get current year for copyright. */
+
+function getCurrentYear() {
+    const currentDate = new Date();
+    return currentDate.getFullYear();
+}
+
+
